3414fffc1778d785d06a6af155e8c4e9d40cefa5
meisnate12 <meisnate12@gmail.com>
2024-08-05 09:40:43 -0400

    remove space

--- check-imdb-hash.py
+++ check-imdb-hash.py
@@ -37,7 +37,7 @@ logger.start()
 keyword = args["keyword"] if args["keyword"] else "Shrek"
 
 folder = os.path.dirname(ChromeDriverManager().install())
-filename = next((f for f in os.listdir(folder) if not f.endswith(".chromedriver ")))
+filename = next((f for f in os.listdir(folder) if not f.endswith(".chromedriver")))
 chrome_driver_path = os.path.join(folder, filename)
 logger.info(f"Keyword: {keyword}")
 logger.info(f"Chrome Driver Path: {chrome_driver_path}")
